ba907b56d9803702345013b12bc271ff39e8f7ce
dear-john <15720788464@163.com>
2018-02-06 21:32:56 +0800

    数据存储工具

---
+++ app/src/main/java/utils/SharedPreferencesUtil.java
@@ -0,0 +1,38 @@
+package utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by 李君 on 2018/2/6.
+ */
+
+public class SharedPreferencesUtil {
+    private static SharedPreferences sPreferences;
+    private static SharedPreferences.Editor sEditor;
+    private static final String DATA_BASE_NAME = "MyDataBase";
+
+    public static void putStringData(Context context, String key, String value) {
+        sPreferences = context.getSharedPreferences(DATA_BASE_NAME, 0);
+        sEditor = sPreferences.edit();
+        sEditor.putString(key, value);
+        sEditor.apply();
+    }
+
+    public static void putIntData(Context context, String key, int value) {
+        sPreferences = context.getSharedPreferences(DATA_BASE_NAME, 0);
+        sEditor = sPreferences.edit();
+        sEditor.putInt(key, value);
+        sEditor.apply();
+    }
+
+    public static String getStringData(Context context, String key) {
+        sPreferences = context.getSharedPreferences(DATA_BASE_NAME, 0);
+        return sPreferences.getString(key, "");
+    }
+
+    public static int getIntData(Context context, String key) {
+        sPreferences = context.getSharedPreferences(DATA_BASE_NAME, 0);
+        return sPreferences.getInt(key, -1);
+    }
+}
