159cb619e1706bc5dfaf6a3b7f4f5746d224fa9b
dear-john <15720788464@163.com>
2018-03-16 17:17:57 +0800

    使用lint优化应用

--- app/src/main/java/app/AppCore.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-/**
- * Created by 李君 on 2018/2/5.
- */
-
-public class AppCore {
-
-}
--- app/src/main/java/base/BaseFragment.java
+++ app/src/main/java/base/BaseFragment.java
@@ -2,16 +2,11 @@ package base;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.List;
-
-import utils.ToastUtil;
 
 
 public abstract class BaseFragment extends Fragment implements View.OnClickListener{
