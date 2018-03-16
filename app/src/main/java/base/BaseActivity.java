90343909da3caa9c2557cb1b56180322badba256
dear-john <15720788464@163.com>
2018-03-16 11:33:38 +0800

    修复侧边栏无法覆盖播放栏的bug

--- app/src/main/java/base/BaseActivity.java
+++ app/src/main/java/base/BaseActivity.java
@@ -6,23 +6,30 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.spring_ballet.lovemusic.MusicPlayService;
 import com.spring_ballet.lovemusic.R;
 
+import adapter.ListViewAdapter;
 import utils.SharedPreferencesUtil;
 import utils.ToastUtil;
 
 public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //控制栏
     private ImageView mSongIv;
     private ImageView mPreIv;
     private ImageView mNextIv;
@@ -30,6 +37,17 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
     private TextView mSongNameTv;
     private TextView mSingerNameTv;
     private View mControllLayout;
+
+    //滑动菜单
+    protected DrawerLayout drawerLayout;
+    private ListView mListView;
+    private ImageView mUserIconIv;
+    private TextView userNameTv;
+    private TextView userLevelTv;
+    private TextView userSignTv;
+    private View mModeLayout;
+    private View mSettingsLayout;
+    private View mQuitLayout;
 
     private static int sSubClassCount;
     private static boolean isFirst = true;
@@ -84,6 +102,7 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
     }
 
     private void initBaseWidgets() {
+        //控制栏
         mSongIv = findViewById(R.id.iv_controll_song_icon);
         mPreIv = findViewById(R.id.iv_controll_music_pre);
         mNextIv = findViewById(R.id.iv_controll_music_next);
@@ -91,13 +110,62 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
         mSongNameTv = findViewById(R.id.tv_controll_song_name);
         mSingerNameTv = findViewById(R.id.tv_controll_singer_name);
         mControllLayout = findViewById(R.id.layout_controll);
+
+        //滑动菜单
+        drawerLayout = findViewById(R.id.draw_layout);
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        mListView = findViewById(R.id.main_listview);
+        mListView.addHeaderView(LayoutInflater.from(this).inflate(R.layout.nav_head, mListView, false));
+        mListView.setAdapter(new ListViewAdapter(this));
+        mListView.setDivider(null);
+        mUserIconIv = findViewById(R.id.nav_head_icon);
+        userNameTv = findViewById(R.id.tv_user_name);
+        userLevelTv = findViewById(R.id.tv_user_level);
+        userSignTv = findViewById(R.id.tv_user_sign);
+        mModeLayout = findViewById(R.id.layout_change_mode);
+        mSettingsLayout = findViewById(R.id.layout_settings);
+        mQuitLayout = findViewById(R.id.layout_quit);
     }
 
     private void initBaseListeners() {
+
+        //控制栏
         mPreIv.setOnClickListener(this);
         mPlayAndPauseIv.setOnClickListener(this);
         mNextIv.setOnClickListener(this);
         mControllLayout.setOnClickListener(this);
+
+        //滑动菜单
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        ToastUtil.showShort(BaseActivity.this, "home page");
+                        break;
+                    case 1:
+                        ToastUtil.showShort(BaseActivity.this, "msg");
+                        break;
+                    case 2:
+                        ToastUtil.showShort(BaseActivity.this, "friend");
+                        break;
+                    case 3:
+                        ToastUtil.showShort(BaseActivity.this, "stop on time");
+                        break;
+                    case 4:
+                        ToastUtil.showShort(BaseActivity.this, "scan");
+                        break;
+                }
+                drawerLayout.closeDrawers();
+            }
+        });
+        userNameTv.setOnClickListener(this);
+        userLevelTv.setOnClickListener(this);
+        userSignTv.setOnClickListener(this);
+        mUserIconIv.setOnClickListener(this);
+        mModeLayout.setOnClickListener(this);
+        mSettingsLayout.setOnClickListener(this);
+        mQuitLayout.setOnClickListener(this);
     }
 
     protected void initControllLayout() {
@@ -137,6 +205,7 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            //控制栏
             case R.id.iv_controll_music_pre:
                 ToastUtil.showShort(this, "pre song");
                 break;
@@ -155,6 +224,29 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
                 break;
             case R.id.layout_controll:
                 ToastUtil.showShort(this, "music controll");
+                break;
+
+            //滑动菜单
+            case R.id.tv_user_name:
+                ToastUtil.showShort(this, "user name");
+                break;
+            case R.id.tv_user_level:
+                ToastUtil.showShort(this, "user level");
+                break;
+            case R.id.tv_user_sign:
+                ToastUtil.showShort(this, "user sign");
+                break;
+            case R.id.nav_head_icon:
+                ToastUtil.showShort(this, "user icon");
+                break;
+            case R.id.layout_change_mode:
+                ToastUtil.showShort(this, "change mode");
+                break;
+            case R.id.layout_settings:
+                ToastUtil.showShort(this, "settings");
+                break;
+            case R.id.layout_quit:
+                finish();
                 break;
         }
     }
@@ -176,6 +268,17 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
             SharedPreferencesUtil.putStringData(this, "singer", sSingerName);
             SharedPreferencesUtil.putStringData(this, "icon", sIcon);
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 }
--- app/src/main/java/com/spring_ballet/lovemusic/MainActivity.java
+++ app/src/main/java/com/spring_ballet/lovemusic/MainActivity.java
@@ -6,19 +6,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import adapter.ListViewAdapter;
 import adapter.MyFragmentAdapter;
 import base.BaseActivity;
 import base.BaseFragment;
@@ -30,25 +24,12 @@ import utils.ToastUtil;
 
 public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
 
-    private DrawerLayout drawerLayout;
-
-    //主界面
     private FrameLayout menuLayout;
     private ImageView localMusicIv;
     private ImageView netMusicIv;
     private ImageView friendIv;
     private FrameLayout searchLauout;
     private ViewPager mainVp;
-
-    //滑动菜单
-    private ListView mListView;
-    private ImageView mUserIconIv;
-    private TextView userNameTv;
-    private TextView userLevelTv;
-    private TextView userSignTv;
-    private View mModeLayout;
-    private View mSettingsLayout;
-    private View mQuitLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,76 +42,28 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
 
     @Override
     protected void initWidgets() {
-        drawerLayout = findViewById(R.id.draw_layout);
         Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.setDisplayShowTitleEnabled(false);
-
-        //主界面
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
         menuLayout = findViewById(R.id.main_menu);
         localMusicIv = findViewById(R.id.iv_local_music);
         netMusicIv = findViewById(R.id.iv_around_music);
         friendIv = findViewById(R.id.iv_friend);
         searchLauout = findViewById(R.id.search);
         mainVp = findViewById(R.id.main_viewpager);
-
-        //滑动菜单
-        mListView = findViewById(R.id.main_listview);
-        mListView.addHeaderView(LayoutInflater.from(this).inflate(R.layout.nav_head, mListView, false));
-        mListView.setAdapter(new ListViewAdapter(this));
-        mListView.setDivider(null);
-        mUserIconIv = findViewById(R.id.nav_head_icon);
-        userNameTv = findViewById(R.id.tv_user_name);
-        userLevelTv = findViewById(R.id.tv_user_level);
-        userSignTv = findViewById(R.id.tv_user_sign);
-        mModeLayout = findViewById(R.id.layout_change_mode);
-        mSettingsLayout = findViewById(R.id.layout_settings);
-        mQuitLayout = findViewById(R.id.layout_quit);
     }
 
     @Override
     protected void initListeners() {
-        //主界面
         menuLayout.setOnClickListener(this);
         localMusicIv.setOnClickListener(this);
         netMusicIv.setOnClickListener(this);
         friendIv.setOnClickListener(this);
         searchLauout.setOnClickListener(this);
         mainVp.addOnPageChangeListener(this);
-
-        //滑动菜单
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        ToastUtil.showShort(MainActivity.this, "home page");
-                        break;
-                    case 1:
-                        ToastUtil.showShort(MainActivity.this, "msg");
-                        break;
-                    case 2:
-                        ToastUtil.showShort(MainActivity.this, "friend");
-                        break;
-                    case 3:
-                        ToastUtil.showShort(MainActivity.this, "stop on time");
-                        break;
-                    case 4:
-                        ToastUtil.showShort(MainActivity.this, "scan");
-                        break;
-                }
-                drawerLayout.closeDrawers();
-            }
-        });
-        userNameTv.setOnClickListener(this);
-        userLevelTv.setOnClickListener(this);
-        userSignTv.setOnClickListener(this);
-        mUserIconIv.setOnClickListener(this);
-        mModeLayout.setOnClickListener(this);
-        mSettingsLayout.setOnClickListener(this);
-        mQuitLayout.setOnClickListener(this);
     }
 
     private void initViewPager() {
@@ -178,27 +111,7 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
             case R.id.search:
                 ToastUtil.showShort(this, "this is search");
                 break;
-            case R.id.tv_user_name:
-                ToastUtil.showShort(this, "user name");
-                break;
-            case R.id.tv_user_level:
-                ToastUtil.showShort(this, "user level");
-                break;
-            case R.id.tv_user_sign:
-                ToastUtil.showShort(this, "user sign");
-                break;
-            case R.id.nav_head_icon:
-                ToastUtil.showShort(this, "user icon");
-                break;
-            case R.id.layout_change_mode:
-                ToastUtil.showShort(this, "change mode");
-                break;
-            case R.id.layout_settings:
-                ToastUtil.showShort(this, "settings");
-                break;
-            case R.id.layout_quit:
-                finish();
-                break;
+
         }
     }
 
@@ -237,17 +150,6 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
     @Override
     public void onPageScrollStateChanged(int state) {
 
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawers();
-                return true;
-            }
-        }
-        return super.onKeyDown(keyCode, event);
     }
 
 }
